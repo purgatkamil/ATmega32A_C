80c306983ba04c3cd29066cb518ff8747b089784
Kamil Purgat <kpurgat.zsp9@gmail.com>
2022-11-22 21:38:43 +0100

    Removing unnecessary comments and cleaning up the code

--- Lab04/main.c
+++ Lab04/main.c
@@ -29,27 +29,12 @@ int B1, B2, B3;
 int previous = 0;
 int mode = 1;
 
-
 void adc_read()
 {
 	W = ADCL;
 	W |= (ADCH << 8);
 	adc = (W * 5 * 10) / 1024;
 }
-
-
-/*int adc_read(char channel)
-{
-      unsigned int W = 0;
-      ADMUX |= channel;
-      ADCSRA |= (1 << ADSC);
-      while(ADCSRA & (1 << ADIF));
-      ADCSRA |= (1 << ADIF);
-      W = ADCL;
-      W |= (ADCH << 8);
-      adc = (W * 5 * 10) / 1024;
-      return adc;
-}*/
 
 void SPI_Init(void)
 {
@@ -60,7 +45,7 @@ void SPI_Init(void)
 void ADC_Init(void)
 {
 	ADMUX = (1 << REFS0);
-	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADIE) ;// | (1 << ADATE) | (1 << ADSC);// | (1 << ADIF);
+	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0) | (1 << ADIE) ;
 }
 
 void AutoTrigger_Init(void)
@@ -75,14 +60,6 @@ char SPI_Transmit(char cData)
 	while(!(SPSR & (1<<SPIF)));
 	return SPDR;
 }
-
-/*void ADC_Init(void)
-{
-      ADMUX = (1 << REFS0);
-      ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
-}*/
-
-
 
 ISR(ADC_vect)
 {
@@ -109,43 +86,26 @@ void setTime(int Hours, int Minutes, int Seconds)
 	dziesiatkiSeconds = dziesiatkiSeconds << 4;
 	wynikSeconds = dziesiatkiSeconds + jednosciSeconds;
 	
-	TWI_Start();                        // start transmission
-	TWI_Write(0xD0);                    // write addres of M41T00
-	TWI_Write(0x00);                    // select hours register                                           // write address for reading data
+	TWI_Start();                // start transmission
+	TWI_Write(0xD0);            // write addres of M41T00
+	TWI_Write(0x00);            // select seconds register
 	TWI_Write(wynikSeconds);	// read hours register
-	TWI_Write(wynikMinutes);	// read hours register                                        // write address for reading data
-	TWI_Write(wynikHours);	// read hours register
-	TWI_Stop();	// stop transmission 
+	TWI_Write(wynikMinutes);	// read hours register
+	TWI_Write(wynikHours);		// read hours register
+	TWI_Stop();					// stop transmission 
 }
 
 void getTime(int *hr,int *min,int *sec)
 {
 	TWI_Start();                        // start transmission
 	TWI_Write(0xD0);                    // write addres of M41T00
-	TWI_Write(0x00);                    // select hours registers
+	TWI_Write(0x00);                    // select seconds registers
 	TWI_Start();                        // repeated start
 	TWI_Write(0xD1);                    // write address for reading data
-	*sec = TWI_Read(ACK);                // read hours register                   // write address for reading data
-	*min = TWI_Read(ACK);                // read hours register                  // write address for reading data
+	*sec = TWI_Read(ACK);               // read seconds register                   
+	*min = TWI_Read(ACK);               // read minutes register                 
 	*hr= TWI_Read(NACK);                // read hours register
-	TWI_Stop();                         // stop transmission
-	
-	/*TWI_Start();                        // start transmission
-      TWI_Write(0xD0);                    // write addres of M41T00
-      TWI_Write(0x01);                    // select hours registers
-      TWI_Start();                        // repeated start
-      TWI_Write(0xD1);                    // write address for reading data
-      *min = TWI_Read(NACK);                // read hours register
-      TWI_Stop(); 
-       
-      TWI_Start();                        // start transmission
-      TWI_Write(0xD0);                    // write addres of M41T00
-      TWI_Write(0x00);                    // select hours registers
-      TWI_Start();                        // repeated start
-      TWI_Write(0xD1);                    // write address for reading data
-      *sec= TWI_Read(NACK);                // read hours register
-      TWI_Stop();  */
-	
+	TWI_Stop();                         // stop transmission	
 }
 
 enum TemperatureScales{Celsius = 0, Fahrenheit = 1};
@@ -168,25 +128,19 @@ int getTemp(int t)
 	
 	if(t == 0)
 	{
-	return A3;
+		return A3;
 	}
 	else if(t == 1) 
 	{
-	A3 = ((A3 * 9) / 5 ) + 32;
-	return A3;
+		A3 = ((A3 * 9) / 5 ) + 32;
+		return A3;
 	}
 	else
 	{
-	A3 = 0;
-	return A3;
+		A3 = 0;
+		return A3;
 	}
 }
-
-/*int getVoltage()
-{
-      int x = adc_read(0);
-      return x;
-}*/
 
 void displayData(int mode, int x, int y)
 {
@@ -232,7 +186,6 @@ void displayData(int mode, int x, int y)
 	LCD_WriteText(".");
 	LCD_GoTo(x+10, y);
 	LCD_WriteData(temperature[2]);
-	
 	break;
 	
 	case 3:
@@ -245,7 +198,6 @@ void displayData(int mode, int x, int y)
 	itoa(B3, str, 20);
 	LCD_GoTo(x+1, y);
 	LCD_WriteData(str[0]);
-	//LCD_WriteText(str);
 
 	LCD_GoTo(x+2,y);
 	LCD_WriteText(":");
@@ -278,51 +230,43 @@ void checkBtn()
 {
 	if (!(PINB & 1))
 	{
-	if(previous == 0)
-	{
-	mode = mode + 1;
-	LCD_WriteCommand(HD44780_CLEAR);
-	previous = 1;
-	if(mode >= 4)
-	{
-	mode = 1;
-	}
-	}
+		if(previous == 0)
+		{
+			mode = mode + 1;
+			LCD_WriteCommand(HD44780_CLEAR);
+			previous = 1;
+			if(mode >= 4)
+			{
+				mode = 1;
+			}
+		}
 	}
 	else
 	{
-	previous = 0;
+		previous = 0;
 	}
 }
 
 int main(void)
 {
 
-DDRD |= (1 << PD1);
+//DDRD |= (1 << PD1);
 
-LCD_Initialize();
-ADC_Init();
-AutoTrigger_Init();
-SPI_Init();
-TWI_Init();
-sei();
-setTime(16, 17, 18);
+	LCD_Initialize();
+	ADC_Init();
+	AutoTrigger_Init();
+	SPI_Init();
+	TWI_Init();
+	sei();
+	setTime(16, 17, 18);
 
-while(1)
-{
-	
-	getTime(&Godziny, &Minuty, &Sekundy);
-	//volt = adc_read(0);// getVoltage();
-	temperaturka = getTemp(Celsius);
-	checkBtn();
-	displayData(mode, 0, 1);
-
-
-
-_delay_ms(100);
+	while(1)
+	{
+		getTime(&Godziny, &Minuty, &Sekundy);
+		temperaturka = getTemp(Celsius);
+		checkBtn();
+		displayData(mode, 0, 1);
+		_delay_ms(100);
 	}
-return 0;
+	return 0;
 }
-//-------------------------------------
-// End of file
-//-------------------------------------
